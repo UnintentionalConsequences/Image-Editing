8e0c0817aa41842723197b2735db9b69206202e8
dataSyncing <mathewschris2017@gmail.com>
2023-10-30 10:30:25 -0500

    Update BufferedImageStack.java

--- src/BufferedImageStack.java
+++ src/BufferedImageStack.java
@@ -1,9 +1,12 @@
-hi
+import java.awt.image.BufferedImage;
+
 public class BufferedImageStack {
 	
+	private BufferedImage[] arrayImage;
 	
-	
-	
+	public BufferedImageStack() {
+		arrayImage = new BufferedImage[2];
+	}
 	
 	
 }
