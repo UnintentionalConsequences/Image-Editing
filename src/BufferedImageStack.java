e5619b500f9515957454d51f979ff6d655ffa5e1
dataSyncing <mathewschris2017@gmail.com>
2023-11-03 10:27:59 -0500

    final change with comments

--- src/BufferedImageStack.java
+++ src/BufferedImageStack.java
@@ -3,16 +3,18 @@ import java.util.EmptyStackException;
 
 public class BufferedImageStack {
 	
-	private BufferedImage[] arrayImage;
-	private int size = 0;
+	// Creating Instance Variables
+	private BufferedImage[] arrayImage; // used as a stack of images
+	private int size = 0; // number of elements that are images
 	
 	public BufferedImageStack() {
 		arrayImage = new BufferedImage[2];
 	}
 	
 	public void push(BufferedImage someBufferedImage) {
-		size++;
+		size++; // since we're adding an image, we're increasing the size
 		
+		// going through the loop and checking to see if there is a null spot
 		for(int i = 0; i<arrayImage.length; i++) {
 			Object check = arrayImage[i];
 			if(!(check instanceof BufferedImage)) {
@@ -21,44 +23,45 @@ public class BufferedImageStack {
 			}
 		}
 		
+		// if there aren't any null spots, then the array is doubled			
 		BufferedImage[] arrayImage2 = new BufferedImage[arrayImage.length*2];
 		
+		// going through the new loop and adding the original images
 		for(int i = 0; i<arrayImage.length; i++) {
 			arrayImage2[i] = arrayImage[i];
 		}
 		
+		// adding the new image into the new array
 		arrayImage2[arrayImage.length] = someBufferedImage;
-		arrayImage = arrayImage2;
+		arrayImage = arrayImage2; // setting the new array(double size) as the image stack
 	}
 	
 	public BufferedImage pop() {
-		size--;
-		BufferedImage temp = arrayImage[size];
-		arrayImage[size] = null;
-		return temp;
+		size--; // decreasing by 1, because one of the images is pop (deleted) from the stack
+		BufferedImage temp = arrayImage[size]; // storing the removed image
+		arrayImage[size] = null; // setting the location to be null
+		return temp; // returns the removed image
 	}
 	
 	public boolean isEmpty() {
-		return (size==0);
+		return (size==0); //checking if there are any images
 	}
 	
 
 	public BufferedImage get(int index) {
 		if(index >= arrayImage.length || index <= -1) {
-			throw new IndexOutOfBoundsException("Index is out of bounds");
+			throw new IndexOutOfBoundsException("Index is out of bounds"); //throwing an exception if the index is out of bounds
 		} else {
-			return arrayImage[index];
+			return arrayImage[index]; // returns the index
 		}
 	}
-	
-	
-	
+		
 	public int getSize() {
-		return size;
+		return size; // returns the number of images
 	}
 	
 	public int getArraySize() {
-		return arrayImage.length;
+		return arrayImage.length; // returns the length of the array stack
 	}
 
 }
